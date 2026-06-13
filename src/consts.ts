91e68830f3ddacc4763f9253a98484319f2b0f41
leaviiiiing <2433179140@qq.com>
2026-06-13 16:44:26 +0800

    🎉 初始化 Astro 博客项目
    - Astro 博客模板 + MDX + Sitemap + RSS
    - Netlify 静态部署适配器
    - 本地字体 (Atkinson Hyperlegible)
    - 5 篇示例文章

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '我的博客';
+export const SITE_DESCRIPTION = '欢迎来到我的博客！记录技术、生活和思考。';
