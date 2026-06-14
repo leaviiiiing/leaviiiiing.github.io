167f42bd6e57b8f18a7959a035f04059cc15b4c6
leaviiiiing <2433179140@qq.com>
2026-06-14 16:48:02 +0800

    Add friend links and remove categories from nav

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -12,15 +12,8 @@ export default defineConfig({
     { href: "/", text: "首页", icon: "i-ri-home-line" },
     {
       text: "文章",
-      href: "/categories/",
+      href: "/tags/",
       icon: "i-ri-quill-pen-fill",
-      dropbox: {
-        enable: true,
-        items: [
-          { href: "/categories/", text: "分类", icon: "i-ri-book-shelf-fill" },
-          { href: "/tags/", text: "标签", icon: "i-ri-price-tag-3-fill" },
-        ],
-      },
     },
     { text: "友链", href: "/friends/", icon: "i-ri-link" },
   ],
@@ -60,6 +53,56 @@ export default defineConfig({
   friends: {
     title: "友链",
     description: "一些有趣的地方",
-    links: [],
+    links: [
+      {
+        url: "https://io-wy.github.io/blog/",
+        title: "io-wy's Blog",
+        desc: "",
+        author: "io-wy",
+        avatar: "",
+      },
+      {
+        url: "https://soapsama.cn/",
+        title: "SoapSama",
+        desc: "",
+        author: "SoapSama",
+        avatar: "",
+      },
+      {
+        url: "https://blog.s3loy.tech/",
+        title: "S3loy's Blog",
+        desc: "",
+        author: "S3loy",
+        avatar: "",
+      },
+      {
+        url: "https://cube1345.github.io/",
+        title: "Cube1345",
+        desc: "",
+        author: "Cube1345",
+        avatar: "",
+      },
+      {
+        url: "https://blog0x76.vercel.app/",
+        title: "0x76's Blog",
+        desc: "",
+        author: "0x76",
+        avatar: "",
+      },
+      {
+        url: "https://blog.ptilopsis.cv/",
+        title: "Ptilopsis",
+        desc: "",
+        author: "Ptilopsis",
+        avatar: "",
+      },
+      {
+        url: "https://seandictionary.top/",
+        title: "Sean's Dictionary",
+        desc: "",
+        author: "Sean",
+        avatar: "",
+      },
+    ],
   },
 });
